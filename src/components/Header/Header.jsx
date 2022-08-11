803345941e6cba7cd0708bb8a3baa748e67d24fc
karimgh <97347938+KarimScript@users.noreply.github.com>
2022-08-11 18:55:14 +0300

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,107 @@
+import React, { useRef, useEffect, useState } from "react";
+
+import { Container } from "reactstrap";
+import logo from "../../assets/images/FoodLogo.svg";
+import { NavLink, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { cartUiActions } from "../../store/shopping-cart/cartUiSlice";
+
+import "../../styles/header.css";
+
+const nav__links = [
+  {
+    display: "Home",
+    path: "/home",
+  },
+  {
+    display: "Foods",
+    path: "/foods",
+  },
+  {
+    display: "Cart",
+    path: "/cart",
+  },
+  {
+    display: "Contact",
+    path: "/contact",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef(null);
+  const headerRef = useRef(null);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+  const dispatch = useDispatch();
+    const [activePage, setactivePage] = useState("Home");
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
+  const toggleCart = () => {
+    dispatch(cartUiActions.toggle());
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("header__shrink");
+      } else {
+        headerRef.current.classList.remove("header__shrink");
+      }
+    });
+
+    return () => window.removeEventListener("scroll");
+  }, []);
+
+  return (
+    <header className="header" ref={headerRef}>
+      <Container>
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          
+          </div>
+
+          {/* ======= menu ======= */}
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <div className="menu d-flex align-items-center gap-5">
+              {nav__links.map((item, index) => (
+                <NavLink
+                  onClick={ () => setactivePage(item.display)}
+                  to={item.path}
+                  key={index}
+                  className={activePage == item.display ? 'active__menu' : '' 
+                  }
+                >
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+          {/* ======== nav right icons ========= */}
+          <div className="nav__right d-flex align-items-center gap-4">
+            <span className="cart__icon" onClick={toggleCart}>
+              <i class="ri-shopping-basket-line"></i>
+              <span className="cart__badge">{totalQuantity}</span>
+            </span>
+
+            <span className="user">
+             
+                <i class="ri-user-line"></i>
+              
+            </span>
+
+            <span className="mobile__menu" onClick={toggleMenu}>
+              <i class="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
